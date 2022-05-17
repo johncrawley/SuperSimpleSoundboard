a99e3d0b51dca9605509469a091a2c7661ec049a
johncrawley <johncrawley1@gmail.com>
2022-05-17 23:42:49 +0100

    Cleaning up unused methods. Also adding map for media players to enable managed polyphonic sounds.

--- app/src/main/java/com/jacstuff/supersimplesoundboard/MainActivity.java
+++ app/src/main/java/com/jacstuff/supersimplesoundboard/MainActivity.java
@@ -6,15 +6,15 @@ import android.media.MediaPlayer;
 import android.media.audiofx.EnvironmentalReverb;
 import android.media.audiofx.PresetReverb;
 import android.os.Bundle;
-import android.os.Environment;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
-import java.io.File;
 import java.io.IOException;
-import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener, MediaPlayer.OnCompletionListener {
 
@@ -31,17 +31,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private Button playButton, reverbButton, fButton, gButton, aButton;
     private MediaPlayer mediaPlayer;
+    private Map<Integer, MediaPlayer> mediaPlayerMap;
+    private List<Integer> playingIds;
     private PresetReverb presetReverb;
     private  android.media.audiofx.EnvironmentalReverb environmentalReverb;
-
+    private final int POLOPHONY = 4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //createSoundboardsDirIfDoesntExist();
-        //listExternalStorage();
-        //1filesDir();
+
+        mediaPlayerMap = new HashMap<>(POLOPHONY);
+        playingIds = new CopyOnWriteArrayList<>();
+
 
         playButton = findViewById(R.id.test_sound_button);
         fButton = findViewById(R.id.f1_button);
@@ -91,21 +94,74 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         @Override
         public void onClick(View v) {
-            if(mediaPlayer.isPlaying()) {
-                mediaPlayer.stop();
-                try {
-                    mediaPlayer.prepare();
-                    mediaPlayer.seekTo(0);
-                    mediaPlayer.start();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                // mediaPlayer.reset();
-                System.out.println("audio session id :" + mediaPlayer.getAudioSessionId());
-            }
+            int id = v.getId();
+            int resId = R.raw.hylophone_a1;
+            playSound(id, resId);
+            resetAndStartIfAlreadyPlaying(mediaPlayer);
             mediaPlayer.start();
         }
     };
+
+
+    private void resetAndStartIfAlreadyPlaying(MediaPlayer mediaPlayer){
+        if(mediaPlayer.isPlaying()) {
+            mediaPlayer.stop();
+            try {
+                mediaPlayer.prepare();
+                mediaPlayer.seekTo(0);
+                mediaPlayer.start();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            // mediaPlayer.reset();
+            System.out.println("audio session id :" + mediaPlayer.getAudioSessionId());
+        }
+        else{
+            mediaPlayer.start();
+        }
+
+    }
+
+    private void playSound(int id, int resId){
+        if(mediaPlayerMap.containsKey(id)){
+            MediaPlayer mediaPlayer = mediaPlayerMap.get(id);
+            if(mediaPlayer != null){
+                resetAndStartIfAlreadyPlaying(mediaPlayer);
+            }
+            removeOldPlayingId(id);
+            playingIds.add(id);
+            return;
+        }
+        if(mediaPlayerMap.size() >= POLOPHONY){
+            int oldestId = playingIds.remove(0);
+            mediaPlayerMap.remove(oldestId);
+        }
+        playingIds.add(id);
+        mediaPlayerMap.put(id, createAndPlay(resId));
+
+    }
+
+
+    private MediaPlayer createAndPlay(int resId){
+        MediaPlayer mediaPlayer = MediaPlayer.create(this, resId);
+        try{
+            mediaPlayer.prepare();
+            mediaPlayer.start();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+        return mediaPlayer;
+    }
+
+    private void removeOldPlayingId(int oldId){
+        Iterator <Integer> iterator = playingIds.iterator();
+        while(iterator.hasNext()){
+            int id = iterator.next();
+            if(id == oldId){
+                iterator.remove();
+            }
+        }
+    }
 
 
     @Override
@@ -120,140 +176,5 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
-
-    private void createSoundboardsDirIfDoesntExist(){
-        String soundBoardsDirName = "Soundboards2/";
-        File externalStorageDirectory = Environment.getExternalStorageDirectory();
-        toast("dir path: " + externalStorageDirectory.getAbsolutePath());
-        File[] filesArray = externalStorageDirectory.listFiles();
-        if(filesArray == null){
-            return;
-        }
-        List<File> files = Arrays.asList(filesArray);
-        for(File f : files){
-            System.out.println("external storage directory files: " + f.getName());
-        }
-        String parentDir = Environment.DIRECTORY_DCIM;
-        File parentDirFile = new File(externalStorageDirectory, parentDir);
-        File soundBoardsDir = new File(parentDirFile, soundBoardsDirName);
-
-        if(soundBoardsDir.exists()){
-            System.out.println("soundboard dir already exists");
-            Toast.makeText(getApplicationContext(),"soundboard Directory exists!",Toast.LENGTH_LONG).show();
-            return;
-        }
-
-        boolean created = soundBoardsDir.mkdir();
-        System.out.println("soundboardsDir absolute path: "  + soundBoardsDir.getAbsolutePath());
-        System.out.println("******* Create soundboardDir result: " + created);
-        Toast.makeText(getApplicationContext(),"Directory exists: " + soundBoardsDir.getAbsolutePath() + " : " + soundBoardsDir.exists(),
-                Toast.LENGTH_LONG).show();
-        Toast.makeText(getApplicationContext(),"Parent Directory exists: " + parentDirFile.getAbsolutePath() + " : " + parentDirFile.exists(),
-                Toast.LENGTH_LONG).show();
-
-
-        File filesDir = getExternalFilesDir(null);
-        File myDir = new File(filesDir, "soundBoards");
-        boolean created2 = myDir.mkdirs();
-        toast("files dir: " + myDir.getAbsolutePath() + " created: " + created2);
-
-
-       // createDir();
-    }
-
-    private void toast(String msg){
-
-        Toast.makeText(getApplicationContext(), msg,
-                Toast.LENGTH_LONG).show();
-    }
-
-    private void setupList(){
-        //ListView list = findViewById(R.id.list1);
-        //listAdapterHelper = new ListAdapterHelper(context, list, this);
-        refreshListFromDb();
-    }
-
-
-    public void refreshListFromDb(){
-    //    List<SimpleListItem> items = db.getAnswerItems(answerPoolId);
-        //listAdapterHelper.setupList(items, android.R.layout.simple_list_item_1, noResultsFoundView);
-    }
-
-     private void createDir(){
-        setContentView(R.layout.activity_main);
-        File file = new File(Environment.getExternalStorageDirectory()+"/Sample Directory");
-        boolean success = true;
-          if(!file.exists()) {
-            Toast.makeText(getApplicationContext(),"Directory does not exist, create it",
-                    Toast.LENGTH_LONG).show();
-        }
-          if(success) {
-            Toast.makeText(getApplication(),"Directory created",
-                    Toast.LENGTH_LONG).show();
-        }
-          else {
-            Toast.makeText(this,"Failed to create Directory",
-                    Toast.LENGTH_LONG).show();
-        }
-    }
-
-
-        private void listExternalStorage() {
-            String state = Environment.getExternalStorageState();
-
-            if (Environment.MEDIA_MOUNTED.equals(state) || Environment.MEDIA_MOUNTED_READ_ONLY.equals(state)) {
-                listFiles(Environment.getExternalStorageDirectory());
-                Toast.makeText(this, "Successfully listed all the files!", Toast.LENGTH_SHORT)
-                        .show();
-            }
-        }
-
-        /**
-         * Recursively list files from a given directory.
-         */
-        private void listFiles(File dir) {
-
-            System.out.println("Entered listFiles()");
-            File[] files = dir.listFiles();
-            if (files != null) {
-                for (File file : files) {
-                    if (file != null) {
-                        System.out.println("file :" + file.getAbsolutePath());
-                    }
-                }
-            }
-            System.out.println("Exiting listFiles()");
-        }
-
-
-        private void listDataDirFiles(){
-            File dataDir = Environment.getRootDirectory();
-            File[] files = dataDir.listFiles();
-            if(files == null){
-                System.out.println("File are null!");
-                return;
-            }
-            for(File f: files){
-                System.out.println("**** " + f.getAbsolutePath());
-            }
-            System.out.println("Environment music :"  + Environment.DIRECTORY_MUSIC);
-        }
-
-
-        private void getMusicDir(){
-            String music = Environment.DIRECTORY_MUSIC;
-            File file = new File(music);
-            listFiles(file);
-
-        }
-
-        private void filesDir(){
-            File[] files = getExternalMediaDirs();
-            File[] fs2  = getExternalFilesDirs(Environment.DIRECTORY_MUSIC);
-            for(File f: fs2){
-                System.out.println("********* " + f.getAbsolutePath());
-                listFiles(f);
-            }
-        }
 
 }

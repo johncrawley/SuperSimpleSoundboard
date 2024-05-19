4d3d43b47c21d2b8aafa0cf0d0becc73bc95ad15
john <johncrawley1@gmail.com>
2024-05-19 23:59:26 +0100

    Adding more functionality to the Song class.

--- app/src/main/java/com/jacstuff/supersimplesoundboard/service/steps/Song.java
+++ app/src/main/java/com/jacstuff/supersimplesoundboard/service/steps/Song.java
@@ -1,13 +1,35 @@
 package com.jacstuff.supersimplesoundboard.service.steps;
 
+import com.jacstuff.supersimplesoundboard.service.SoundHolder;
+import com.jacstuff.supersimplesoundboard.service.SoundPlayer;
 import com.jacstuff.supersimplesoundboard.service.preferences.PreferencesManager;
+import com.jacstuff.supersimplesoundboard.view.MainView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Song {
 
     private List<SongPart> songParts;
+    private int currentIndex;
     private SongPart currentSongPart;
+    private MainView view;
+    private StepPlayer stepPlayer;
+
+    public Song(List<SoundHolder> soundHolders, int numberOfParts, StepPlayer stepPlayer){
+        songParts = new ArrayList<>(numberOfParts);
+        this.stepPlayer = stepPlayer;
+        for(int i = 0; i < numberOfParts; i++){
+            songParts.add(new SongPart(16, soundHolders));
+        }
+        assignCurrentSongPart();
+    }
+
+
+    private void setView(MainView view){
+        this.view = view;
+    }
+
 
     public void loadSavedSteps(PreferencesManager preferencesManager){
 
@@ -20,6 +42,30 @@ public class Song {
     public void saveSteps(PreferencesManager preferencesManager){
 
     }
+
+
+    public void loadNextSongPart(){
+        incrementCurrentIndex();
+        assignCurrentSongPart();
+    }
+
+
+    private void assignCurrentSongPart(){
+        currentSongPart = songParts.get(currentIndex);
+        currentSongPart.setView(view);
+        currentSongPart.updateView();
+    }
+
+
+    private void incrementCurrentIndex(){
+        currentIndex = currentIndex > songParts.size() -1 ? 0 : currentIndex + 1;
+    }
+
+
+    private void decrementCurrentIndex(){
+        currentIndex = currentIndex - 1 < 0 ? songParts.size() -1 : currentIndex -1;
+    }
+
 
 
 

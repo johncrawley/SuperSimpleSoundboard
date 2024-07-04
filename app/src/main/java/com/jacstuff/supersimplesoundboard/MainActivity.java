ad20db1c284531c38b7873f47ebe57bb0bfb575a
john <johncrawley1@gmail.com>
2024-07-04 23:57:49 +0100

    Added buttons and permissions code to main activity to record and playback audio.

--- app/src/main/java/com/jacstuff/supersimplesoundboard/MainActivity.java
+++ app/src/main/java/com/jacstuff/supersimplesoundboard/MainActivity.java
@@ -1,12 +1,18 @@
 package com.jacstuff.supersimplesoundboard;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.content.ComponentName;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.media.MediaPlayer;
+import android.media.MediaRecorder;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.view.View;
@@ -17,10 +23,13 @@ import android.widget.LinearLayout;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
+
+import static android.Manifest.permission.RECORD_AUDIO;
 import com.jacstuff.supersimplesoundboard.service.SoundBoardServiceImpl;
 import com.jacstuff.supersimplesoundboard.service.SoundBoardService;
 import com.jacstuff.supersimplesoundboard.view.MainView;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.atomic.AtomicBoolean;
@@ -58,7 +67,9 @@ public class MainActivity extends AppCompatActivity implements MainView {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestPermissions();
         setContentView(R.layout.activity_main);
+        setupRecordingButtons();
         assignButtonLayout();
         startService();
         setupSoundButtons();
@@ -296,5 +307,134 @@ public class MainActivity extends AppCompatActivity implements MainView {
         buttonParams.setMargins(4,4,4,4);
     }
 
+
+    private static final int REQUEST_RECORD_AUDIO_PERMISSION = 200;
+    private static String fileName = null;
+
+
+    private MediaRecorder recorder = null;
+
+    private MediaPlayer player = null;
+
+    // Requesting permission to RECORD_AUDIO
+    private boolean permissionToRecordAccepted = false;
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode){
+            case REQUEST_RECORD_AUDIO_PERMISSION:
+                permissionToRecordAccepted  = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                break;
+        }
+        if (!permissionToRecordAccepted ) finish();
+
+    }
+
+    private final ActivityResultLauncher<String> recordAudioPermissionLauncher = registerForActivityResult(
+            new ActivityResultContracts.RequestPermission(),
+            result -> {
+                if (result) {
+
+                }
+            }
+    );
+
+    private Button recordSoundButton, playRecordingButton;
+    private ViewGroup recordButtonsLayout;
+    private boolean isRecording;
+    private boolean isPlaying;
+
+
+    private void setupRecordingButtons(){
+        recordSoundButton = findViewById(R.id.recordSoundButton);
+        recordSoundButton.setOnClickListener(v -> onRecord());
+
+        playRecordingButton = findViewById(R.id.playRecordingButton);
+        playRecordingButton.setOnClickListener(v -> {
+            if(isRecording){
+                stopRecording();
+            }
+            playRecordedSound();
+        } );
+    }
+
+    private void requestPermissions(){
+        askPermissionFor(RECORD_AUDIO, recordAudioPermissionLauncher);
+    }
+
+
+    private void askPermissionFor(String permission, ActivityResultLauncher<String> resultLauncher){
+        if(checkSelfPermission(permission) != PackageManager.PERMISSION_GRANTED){
+            resultLauncher.launch(permission);
+        }
+    }
+
+
+    private void onRecord() {
+        if (isRecording) {
+            startRecording();
+        } else {
+            stopRecording();
+        }
+    }
+
+
+    private void playRecordedSound() {
+        if (isPlaying) {
+            startPlaying();
+        } else {
+            stopPlaying();
+        }
+    }
+
+
+    private void startPlaying() {
+        player = new MediaPlayer();
+        try {
+            player.setDataSource(fileName);
+            player.prepare();
+            player.start();
+        } catch (IOException e) {
+            log("start playing prepare() failed");
+        }
+    }
+
+
+    private void stopPlaying() {
+        player.release();
+        player = null;
+    }
+
+
+    private void startRecording() {
+        isRecording = true;
+        recorder = new MediaRecorder();
+        recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        recorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        recorder.setOutputFile(fileName);
+        recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+
+        try {
+            recorder.prepare();
+            recorder.start();
+        } catch (IOException e) {
+           log("startRecording() prepare() failed");
+           isRecording = false;
+        }
+    }
+
+
+    private void log(String msg){
+        System.out.println("^^^ MainActivity: " + msg);
+    }
+
+
+    private void stopRecording() {
+        recorder.stop();
+        recorder.release();
+        recorder = null;
+        isRecording = false;
+    }
 
 }
